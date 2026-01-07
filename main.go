c5c1bc1d8039516ad47ec6677aed535463171530
Stèlios Stigter <sstigter@tilaa.com>
2026-01-07 10:43:38 +0100

    Added cobra to give the application commands

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/gjsstigter/ssh_config/cmd"
+	"github.com/gjsstigter/ssh_config/config"
 )
 
 func main() {
-	fmt.Println("SSH Config Parser Example")
+	fmt.Println("=== SSH Config Parser ===")
+	config.Initialize()
+
+	if err := config.LoadConfig(); err != nil {
+		fmt.Printf("Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.SetFlags(0)
+	cmd.Execute()
 }
